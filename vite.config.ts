3a710b08eb3cbacf4ff41f8b241bd547982ea6dd
Wieland Morgenstern <wieland.morgenstern@hhi.fraunhofer.de>
2025-05-22 19:35:57 +0200

    disable soruce-maps and license banners for prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: 'public',             // serve index.html from /public
   server: { port: 5173 },
-  build: { outDir: '../dist' }, // optional "npm run build"
+  build: {
+    outDir: '../dist', // optional "npm run build"
+    sourcemap: false,
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+    },
+  },
   plugins: [react()]
 });
